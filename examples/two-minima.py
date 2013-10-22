6744e831979792fc6900dc9d60fa0b4be4fe006e
Jakub Konka <kubkon@gmail.com>
2013-10-22 21:45:36 +0100

    Add example of function with two relative minima..

--- examples/two-minima.py
+++ examples/two-minima.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # Define objective function
 def objective(xs):
     x1, x2 = xs[0], xs[1]
-    return x1**2 + x2**2 - 3*x1 - x1*x2 + 3
+    return x1**4 + x2**4 - 1.25 * x1**2 + 0.25
 
 # Define callback function
 simplices = []
@@ -14,10 +14,11 @@ def callback(args):
     simplices.append(args[0])
 
 # Initial simplex
-simplex = np.array([[0,0], [0,1], [1,0]], dtype=np.float)
+simplex = np.array([[-2,0], [-1,2], [-1.75,2]], dtype=np.float)
+simplex = np.array([[2,0], [1,2], [1.75,2]], dtype=np.float)
 
 # Initialise NelderMead simplex algorithm
-nm = NelderMeadSimplex(objective, simplex, epsilon=1e-1, callback=callback)
+nm = NelderMeadSimplex(objective, simplex, epsilon=1e-6, callback=callback)
 
 # Minimise the objective function
 solution = nm.solve()
@@ -25,15 +26,15 @@ solution = nm.solve()
 print("Minimum at {}".format(solution))
 
 # Tabulate objective function
-x = np.linspace(-1, 3, 1000)
-y = np.linspace(-2, 2, 1000)
+x = np.linspace(-3, 3, 1000)
+y = np.linspace(-3, 3, 1000)
 X, Y = np.meshgrid(x, y)
-Z = X**2 + Y**2 - 3*X - X*Y + 3
+Z = X**4 + Y**4 - 1.25 * X**2 + 0.25
 
 # Plot function contours together with the evolution of
 # the simplices as they approach the minimum
-plt.figure()
-cs = plt.contour(X, Y, Z, 25)
+fig = plt.figure()
+cs = plt.contour(X, Y, Z, 20)
 plt.clabel(cs, inline=1, fontsize=10)
 
 for simplex in simplices:
@@ -47,4 +48,4 @@ for simplex in simplices:
             plt.plot(*zip(simplex[i], simplex[j]), c='black')
 
 plt.grid()
-plt.savefig('one-minimum.pdf')
+plt.savefig('two-minima.pdf')
